b1e3418bace90bce5608ea2809738279557be66b
Mathew Topper <damm_horse@yahoo.co.uk>
2017-09-14 10:20:08 +0100

    Fix libmagic importing for win64

--- dtocean_qt/encoding.py
+++ dtocean_qt/encoding.py
@@ -1,13 +1,21 @@
-import sys
-import os
 
+import os
+import sys
+import struct
+
+BITS = struct.calcsize("P") * 8
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
-if sys.platform == 'win32':
+if sys.platform == 'win32' and BITS == 32:
     # add local folder to path
-    lib = os.path.join(BASEDIR, '_lib', 'magic')
+    MAGICDIR = os.path.join(BASEDIR, '_lib', 'magic', 'win32')
     envpath = os.environ['PATH']
-    os.environ['PATH'] = ';'.join([lib, envpath])
+    os.environ['PATH'] = ';'.join([MAGICDIR, envpath])
+elif sys.platform == 'win32' and BITS == 64:
+    # add local folder to path
+    MAGICDIR = os.path.join(BASEDIR, '_lib', 'magic', 'win64')
+    envpath = os.environ['PATH']
+    os.environ['PATH'] = ';'.join([MAGICDIR, envpath])
 
 try:
     import magic
@@ -26,7 +34,7 @@ class Detector(object):
                 # use the system wide installed version comming with windows, the included magic.mgc might be incompatible
                 magic_db = os.path.join('/usr/share/file', 'magic.mgc')
             else:
-                magic_db = os.path.join(BASEDIR, '_lib', 'magic', 'db', 'magic.mgc')
+                magic_db = os.path.join(MAGICDIR, 'db', 'magic.mgc')
             if not os.path.exists(magic_db):
                 raise ImportError('Please install libmagic.')
             self.magic = magic.Magic(magic_file=magic_db, mime_encoding=True)
